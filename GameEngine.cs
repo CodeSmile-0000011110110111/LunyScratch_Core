c4e61dc457f5114d424341821a4e0fe9fecf3ebf
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-10-06 20:48:33 +0200

    again

--- GameEngine.cs
+++ GameEngine.cs
@@ -1,24 +1,25 @@
 ﻿using System;
 
-namespace LunyScratch;
-
-internal static class GameEngine
+namespace LunyScratch
 {
-	private static IEngineRuntime? s_Runtime;
-	private static IEngineActions? s_Actions;
-
-	internal static IEngineRuntime Runtime => s_Runtime ?? throw new Exception("Scratch Engine: Runtime not initialized");
-	internal static IEngineActions Actions => s_Actions ?? throw new Exception("Scratch Engine: Actions not initialized");
-
-	internal static void Initialize(IEngineRuntime runtime, IEngineActions actions)
+	internal static class GameEngine
 	{
-		s_Runtime = runtime;
-		s_Actions = actions;
-	}
+		private static IEngineRuntime? s_Runtime;
+		private static IEngineActions? s_Actions;
 
-	internal static void Shutdown()
-	{
-		s_Runtime = null;
-		s_Actions = null;
+		internal static IEngineRuntime Runtime => s_Runtime ?? throw new Exception("Scratch Engine: Runtime not initialized");
+		internal static IEngineActions Actions => s_Actions ?? throw new Exception("Scratch Engine: Actions not initialized");
+
+		internal static void Initialize(IEngineRuntime runtime, IEngineActions actions)
+		{
+			s_Runtime = runtime;
+			s_Actions = actions;
+		}
+
+		internal static void Shutdown()
+		{
+			s_Runtime = null;
+			s_Actions = null;
+		}
 	}
 }
