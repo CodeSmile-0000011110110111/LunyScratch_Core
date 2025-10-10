83aaab469a12bb571aa6efa900f881c5bdfc999b
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-10-10 13:33:59 +0200

    update log

--- Blocks/Diagnostics/Blocks.Diagnostics.cs
+++ Blocks/Diagnostics/Blocks.Diagnostics.cs
@@ -5,5 +5,7 @@ namespace LunyScratch
 	public static partial class Blocks
 	{
 		public static IScratchBlock Log(String message) => new ExecuteBlock(() => GameEngine.Actions.LogInfo(message));
+		public static IScratchBlock LogWarn(String message) => new ExecuteBlock(() => GameEngine.Actions.LogWarn(message));
+		public static IScratchBlock LogError(String message) => new ExecuteBlock(() => GameEngine.Actions.LogError(message));
 	}
 }
--- Table.cs
+++ Table.cs
@@ -66,7 +66,7 @@ namespace LunyScratch
 				current = initialValue;
 				_dictionary[key] = current;
 			}
-			
+
 			if (current.Type != ValueType.String)
 			{
 				current.Increment(delta);
